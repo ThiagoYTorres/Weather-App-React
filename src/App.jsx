ac111d9ab034cc61bff4d84e768fa98eabd6c530
Thiago Yashimura Torres <thiagoyashimura03@gmail.com>
2025-12-06 22:23:59 -0300

    arquivos iniciais

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import React, {useState} from 'react';
+
+function App() {
+
+  const [weather, setWeather] = useState()
+  const [estado, setEstado] = useState()
+  const [lugar, setLugar] = useState('')
+  const key = process.env.REACT_APP_API_KEY
+
+  const language = 'pt'
+  
+  React.useEffect( () => {
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${lugar}&limit=3&appid=${key}&lang=${language}`)
+      .then( resp => resp.json())
+      .then( data  => {
+        setEstado(data)
+        console.log(data)
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${data[0].lat}&lon=${data[0].lon}&appid=${key}&units=metric`)
+          .then( resp => resp.json())
+          .then( data2 => {
+          console.log(data2)
+          setWeather(data2)
+        })
+      })
+      
+  },[lugar])
+
+  function searchWeather(formData){
+    const input = formData.get('lugar')
+    setLugar( prev => prev = input)
+    console.log(lugar)
+  }
+  return (
+   
+    <main>
+      <div className='clima-search'>
+        <form action={searchWeather}>
+          <input type='text' className='lugar' name='lugar'></input>
+        </form>
+        {console.log(lugar)}
+        <span className="material-symbols-outlined s" >search</span>
+      </div>
+      
+      {weather && <section className='clima'>
+        <div className='clima-content'>
+            <div className='location'>
+              <span className='loc'>{weather.name}, {estado[0].state}</span>
+              <span className='country'>{weather.sys.country}</span>
+            </div>
+              
+            <div>
+              <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} 
+              className='weather-icon'></img> 
+              <h1>{weather.main.temp} °C</h1>
+            </div>
+        </div>
+        
+        
+        
+        </section>}
+    </main>
+  )
+}
+
+export default App
